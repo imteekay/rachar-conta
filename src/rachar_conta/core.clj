66168476d38ab34eed101434bcfad958f78c32c0
TK <leandrotk100@gmail.com>
2018-10-30 20:38:03 -0300

    Add missing builder

--- src/rachar_conta/core.clj
+++ src/rachar_conta/core.clj
@@ -1,5 +1,6 @@
 (ns rachar-conta.core
   (:require [rachar-conta.calculation :as calculation]
-            [rachar-conta.persistence :as persistence]))
+            [rachar-conta.persistence :as persistence]
+            [rachar-conta.builders :as builders]))
 
 (def bill-card (atom []))
