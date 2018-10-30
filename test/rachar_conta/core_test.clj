a78ac67925b695d8ca9cda3650e3930d6a9d9b04
TK <leandrotk100@gmail.com>
2018-10-30 20:39:35 -0300

    Remove core test

--- test/rachar_conta/core_test.clj
+++
@@ -1,5 +0,0 @@
-(ns rachar-conta.core-test
-  (:require [clojure.test :refer :all]
-            [rachar-conta.calculation :as calculation]
-            [rachar-conta.persistence :as persistence]
-            [rachar-conta.core :refer :all]))
