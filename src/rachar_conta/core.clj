042ebfcdbf7e192f29fb5170e3ca2ccc50eae060
TK <leandrotk100@gmail.com>
2018-10-31 18:14:26 -0300

    Using let for print-show-separated-bills and fix customer price

--- src/rachar_conta/core.clj
+++ src/rachar_conta/core.clj
@@ -13,14 +13,14 @@
     (builders/build-order food customer price)))
 
 (defn -main [& args]
-  (order bill-card "Simple Burger" "Kaio" 22.00)
-  (order bill-card "X-Bacon"       "TK" 29.00)
+  (order bill-card "Simple Burger" "Kaio"   22.00)
+  (order bill-card "X-Bacon"       "TK"     29.00)
   (order bill-card "Veggie Burger" "Kazumi" 30.00)
   (order bill-card "X-Burger"      "Monica" 24.00)
-  (order bill-card "Coke"          "Kaio" 5.00)
+  (order bill-card "Coke"          "Kaio"   5.00)
   (order bill-card "Heineken"      "Kazumi" 7.00)
-  (order bill-card "Coke"          "Kaio" 5.00)
-  (order bill-card "Coke"          "Kaio" 5.00)
+  (order bill-card "Coke"          "Kaio"   5.00)
+  (order bill-card "Coke"          "Monica" 5.00)
 
   (visualization/print-show-total-bill (calculation/total-bill @bill-card))
 
@@ -29,6 +29,7 @@
       (calculation/total-bill @bill-card) 4))
 
   (visualization/print-show-separated-bills
-    (builders/build-price-per-customer
-      ["Kaio" "TK" "Kazumi" "Monica"]
-      @bill-card)))
+    (let [customers ["Kaio" "TK" "Kazumi" "Monica"]]
+      (builders/build-price-per-customer
+        customers
+        @bill-card))))
